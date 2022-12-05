0efa6c7511eb3b5d176998463fae2ed68a20495d
weigefenxiang <119787190+weigefenxiang@users.noreply.github.com>
2022-12-05 21:54:29 +0800

    Update start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -41,5 +41,5 @@ iptables -t nat -A PREROUTING -j TCP_REDIR                        #转发来自�
 
 # 开始
 #/usr/local/bin/clash -d /root/.config/clash
-nohup clash >/root/.config/clash/logs/log.txt 2>&1 &
+nohup /usr/local/bin/clash -d /root/.config/clash >>/root/.config/clash/logs/log.txt 2>&1 &
 
